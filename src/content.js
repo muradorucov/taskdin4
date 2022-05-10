00d6f2b9707875c93f24fe6beaa6b3e40d61154c
muradorucov <orucowmurad@gmail.com>
2022-05-10 11:39:30 +0400

    content item

--- src/content.js
+++ src/content.js
@@ -1,64 +1,35 @@
 import React from "react";
 
 export default class Content extends React.Component{
+    state = {
+        data: []
+    };
+    getData=()=>{
+        fetch("https://6278b7bcd00bded55add177e.mockapi.io/api/taskdin/content")
+        .then(res=>res.json())
+        .then(data=>{
+            this.setState({ data: data })       
+        })
+    }
+    componentDidMount(){
+        this.getData();
+    }
     render(){
+        const { data } = this.state;
         return(
-            <div class="content">
-                        <div class="content-item">
-                            <div class="image yellow"></div>
-                            <div class="desc">
-                                <p>Getting Started</p>
-                                <div class="btn">
-                                    <span>COMPLETED</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="content-item">
-                            <div class="image red"></div>
-                            <div class="desc">
-                                <p>Getting Started</p>
-                                <div class="btn">
-                                    <span>COMPLETED</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="content-item">
-                            <div class="image blue"></div>
-                            <div class="desc">
-                                <p>Getting Started</p>
-                                <div class="btn">
-                                    <span>COMPLETED</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="content-item">
-                            <div class="image green"></div>
-                            <div class="desc">
-                                <p>Getting Started</p>
-                                <div class="btn">
-                                    <span>COMPLETED</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="content-item">
-                            <div class="image yellow-white"></div>
-                            <div class="desc">
-                                <p>Getting Started</p>
-                                <div class="btn">
-                                    <span>COMPLETED</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="content-item ">
-                            <div class="image red-white"></div>
-                            <div class="desc">
-                                <p>Getting Started</p>
-                                <div class="btn in-progres">
-                                    <span>0%</span>
-                                </div>
+            <div className="content">
+                {data.map(item =>
+                    <div className="content-item">
+                        <div className={item.colorClass}></div>
+                        <div className="desc">
+                            <p>Getting Started</p>
+                            <div className="btn">
+                                <span>COMPLETED</span>
                             </div>
                         </div>
                     </div>
+                )}
+            </div>   
         )
     }
 }
--- src/menu.js
+++ src/menu.js
@@ -1,28 +1,10 @@
 import React from "react";
 
 export default class Menu extends React.Component{
-    state = {
-        data: []
-    };
-    getData=()=>{
-        fetch("https://6278b7bcd00bded55add177e.mockapi.io/api/taskdin/menu")
-        .then(res=>res.json())
-        .then(data=>{
-            this.setState({ data: data })       
-        })
-    }
-    componentDidMount(){
-        this.getData();
-    }
     render(){
-        const { data } = this.state;
         return(
             <div className="header">
-                {data.map(item =>
-                        <p key={item.id}>
-                            {item.name}
-                        </p>
-                    )}
+                <p >Home</p>
             </div>
         )
     }
